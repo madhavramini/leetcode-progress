dc08e9a2622f3eb2d1b37b380f868eb9b0498b13
Madhav Ramini <madhavramini.workspace@gmail.com>
2025-07-20 08:27:46 -0700

    Create 200-NumberOfIslands

---
+++ 200-NumberOfIslands/200-NumberOfIslands.cpp
@@ -0,0 +1,30 @@
+// Last updated: 20/07/2025, 20:57:45
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0')
+            return;
+        
+        grid[i][j] = '0'; // mark as visited
+
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty() || grid[0].empty()) return 0;
+
+        int count = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+};
